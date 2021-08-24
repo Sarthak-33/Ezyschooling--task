9ccafb6c6bb47749bbaaed30ba2f732caf088697
Sarthak Pandey <sarthak.x.pandey3313@gmail.com>
2021-08-24 15:25:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <h1 id="header1">The School Genie <span>Where fertile minds find nourishing soils.</span></h1>
     
     <div className="App">
-
+//     Doing basic routing for a single-page-application
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/Home/:id" component={Post} />
